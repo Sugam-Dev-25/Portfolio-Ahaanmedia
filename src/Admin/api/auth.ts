7e5ae00f03b0e6d785d6fc0b193b952ed7461920
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-05-19 18:22:24 +0530

    last deploy

--- src/Admin/api/auth.ts
+++
@@ -1,8 +0,0 @@
-// src/api/auth.ts
-import client from './client';
-import type { LoginResponse } from '../types';
-
-export const login = async (email: string, password: string): Promise<LoginResponse> => {
-  const response = await client.post<LoginResponse>('/auth/login', { email, password });
-  return response.data;
-};
